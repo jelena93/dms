a00edf05e556371939b4b903c3af36ec6972fb27
hachiko93 <ana_93_bg@hotmail.com>
2017-01-15 20:13:30 +0100

    SearchController added (rest)

--- src/main/java/org/nst/dms/controllers/LoginController.java
+++ src/main/java/org/nst/dms/controllers/LoginController.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
